3fc16e23be0b2e1d84e996c660985ec56931aac9
gen2-eslam <78043455+gen-eslam@users.noreply.github.com>
2026-04-19 02:43:02 +0200

    feat: add lightBlue color and implement See All button in MyFavoriteTracksView

--- src/app/features/(drawer)/home/componant/my_favorite_tracks_view.tsx
+++ src/app/features/(drawer)/home/componant/my_favorite_tracks_view.tsx
@@ -1,12 +1,12 @@
 import AppColor from "@/core/utils/app_color";
 import AppFontsFamily from "@/core/utils/app_fonts";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useMyFavoriteTracks } from "../hooks/tracks_hooks";
 import { FlatListErrorView } from "./flat_list_error_view";
 import { FlatListLoadingView } from "./flat_list_loading_view";
 import { HorizontalFlatList } from "./horizontal_flat_list";
 
- export const MyFavoriteTracksView = () => { 
+export const MyFavoriteTracksView = () => {
   const { data, loading, error, getTracksData } = useMyFavoriteTracks();
   if (loading) {
     return <FlatListLoadingView />;
@@ -16,17 +16,31 @@ import { HorizontalFlatList } from "./horizontal_flat_list";
   }
   return (
     <View style={{ gap: 10 }}>
-      <Text style={styles.text}>My Favorite Tracks</Text>
+      <View style={styles.row}>
+        <Text style={styles.text}>My Favorite Tracks</Text>
+        <TouchableOpacity onPress={() => {}}>
+          <Text style={styles.seeAllText}>See All</Text>
+        </TouchableOpacity>
+      </View>
       <HorizontalFlatList data={data} />
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     fontFamily: AppFontsFamily.bold,
     color: AppColor.dark,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  seeAllText: {
+    fontSize: 16,
+    fontFamily: AppFontsFamily.bold,
+    color: AppColor.lightBlue,
   },
 });
